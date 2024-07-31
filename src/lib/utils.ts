894a7fe0dda88edfd49cbcf8566448bebe649f70
Amy Henke <amyleehenke1@gmail.com>
2024-07-31 16:27:42 +0100

    add cart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,24 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function formatPrice(
+    price: number | string,
+    options: {
+        currency?: "USD" | "EUR" | "GBP" | "BDT"
+        notation?: Intl.NumberFormatOptions["notation"]
+    } = {}
+) {
+    const { currency = "GBP", notation = "compact" } = options
+
+    const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+    return new Intl.NumberFormat("en-UK", {
+        style: "currency",
+        currency: currency,
+        notation: notation,
+        maximumFractionDigits: 2,
+    }).format(numericPrice)
 }
